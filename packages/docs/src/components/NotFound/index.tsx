8a71a9fd2b51b4b0302a963c2ad208ac441d32ac
Stephen Zhou <hi@hyoban.cc>
2023-11-05 17:00:57 +0800

    chore: remove docs

--- packages/docs/src/components/NotFound/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Callout } from "../Callout"
-
-export function NotFound() {
-  return (
-    <Callout type="danger" title="404">
-      Page Not Found
-    </Callout>
-  )
-}
--- packages/docs/src/vite-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="servite/global" />
-/// <reference types="unplugin-icons/types/react" />
