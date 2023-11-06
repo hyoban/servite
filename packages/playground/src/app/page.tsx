c52765e02af7d1bc650b48ee90069964847d66e0
Stephen Zhou <hi@hyoban.cc>
2023-11-06 18:38:50 +0800

    chore: update demo

--- packages/playground/src/app/page.tsx
+++ packages/playground/src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Helmet } from "servite/client"
+
 export default function Page() {
-  return <div>hello</div>
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      hello
+    </div>
+  )
 }
