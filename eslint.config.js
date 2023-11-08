be551e33dd4c2355bdf60f36efefe9985b08200a
Stephen Zhou <hi@hyoban.cc>
2023-11-09 00:21:31 +0800

    chore: update lint and format config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,66 @@
-import { planetMatrix } from "@planet-matrix/eslint-config"
+// @ts-check
+import process from "node:process"
 
-export default planetMatrix({
-  configs: [
-    {
-      ignores: ["packages/playground/**/*"],
+import ts from "@typescript-eslint/eslint-plugin"
+// @ts-expect-error
+import tsParser from "@typescript-eslint/parser"
+import { defineFlatConfig } from "eslint-define-config"
+
+const GLOB_EXCLUDE = [
+  "**/node_modules",
+  "**/dist",
+  "**/package-lock.json",
+  "**/yarn.lock",
+  "**/pnpm-lock.yaml",
+
+  "**/output",
+  "**/coverage",
+  "**/temp",
+  "**/.vitepress/cache",
+  "**/.nitro",
+  "**/.nuxt",
+  "**/.next",
+  "**/.vercel",
+  "**/.changeset",
+  "**/.idea",
+  "**/.output",
+  "**/.vite-inspect",
+
+  "**/CHANGELOG*.md",
+  "**/*.min.*",
+  "**/LICENSE*",
+  "**/__snapshots__",
+  "**/auto-import?(s).d.ts",
+  "**/components.d.ts",
+]
+
+export const GLOB_EXCLUDE_ROUTING_FILE = [
+  "**/app/**/*.tsx",
+  "**/server/**/*.ts",
+]
+
+export default defineFlatConfig([
+  {
+    ignores: GLOB_EXCLUDE,
+  },
+  {
+    ignores: ["packages/playground/**/*"],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
     },
-  ],
-})
+    plugins: {
+      "@typescript-eslint": ts,
+    },
+    rules: {
+      ...ts.configs["eslint-recommended"]?.overrides?.[0]?.rules,
+      ...ts.configs["recommended"]?.rules,
+
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
+])
---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+/** @type {import('@ianvs/prettier-plugin-sort-imports').PrettierConfig} */
+export default {
+  semi: false,
+  overrides: [
+    {
+      files: "pnpm-lock.yaml",
+      options: {
+        requirePragma: true,
+      },
+    },
+  ],
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^~/(.*)$",
+    "",
+    "^[.]",
+    "",
+    "<TYPES>",
+  ],
+  importOrderTypeScriptVersion: "5.0.0",
+}
